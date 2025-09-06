3d8a9bac053354a900fd2d2055883a9b63741de9
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-09-06 19:32:19 +0700

    e404_attr: allow skipping kgsl page pool zeroing
    Co-authored-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Co-authored-by: kenaidi01 <andrianp920@gmail.com>
    Signed-off-by: Andriannn <andrianp920@gmail.com>

---
+++ include/linux/lunar_attributes.h
@@ -0,0 +1,15 @@
+#ifndef _LINUX_LUNAR_ATTRIBUTES_H
+#define _LINUX_LUNAR_ATTRIBUTES_H
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+
+struct lunar_attributes {
+    int lunar_kgsl_skip_zeroing;
+};
+
+extern struct lunar_attributes lunar_data;
+#endif /* _LINUX_LUNAR_ATTRIBUTES_H */
